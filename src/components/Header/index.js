44f2e087f39803ca3aa8b62869dda9e315675f7e
Victor-saad <124303726+Victor-saad@users.noreply.github.com>
2023-10-24 10:40:04 -0300

    ajuste width logo mobile & desktop

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,11 +4,11 @@ import Navbar from "./navbar/navbar";
 
 const LogoHeader = styled.img`
     @media screen and (max-width: 750px) {
-        width: 100vw;
+        max-width: 100%;
     }
     
     @media screen and (min-width: 751px) {
-        width: 100vw;
+        max-width: 100%;
 
     }
 `

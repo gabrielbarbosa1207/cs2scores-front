e10308100128ea3c50fb0a1badafb2c4511a313c
Victor-saad <124303726+Victor-saad@users.noreply.github.com>
2023-10-24 16:13:04 -0300

    Estilização NavBar, alterar selected no 1º click

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,7 @@ import Navbar from "./navbar/navbar";
 
 const LogoHeader = styled.img`
     @media screen and (max-width: 750px) {
-        max-width: 100%;
+        width: 100%;
     }
     
     @media screen and (min-width: 751px) {
--- src/components/Header/navbar/navbar.js
+++ src/components/Header/navbar/navbar.js
@@ -1,42 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const NavBar = styled.nav`
     display: flex;
-    flex-direction: row;
+    margin: 23px auto;
+    min-width: 75%;
     align-items: center;
-    justify-content: space-evenly;
-    height: 50px;
-    font-size: 24px;
-    background-Color: #252525;
-    width: 75%;
-    margin: 20px auto;
-    border-radius: 20px;
-    `
+    justify-content: center;
+    background-color: #252525;
+    border-radius: 16px;
+    a {
+        width: 100%;
+        text-align: center;
+        font-size: 16px;
+        padding: 0;
+        text-decoration: none;
+        color: white;
+    }
+    `;
+    
+    const NavBarItems = styled.div`
+        padding: 20px 0;
+        background-color: ${({ selected }) => selected ? 'red' : 'inherit'};
+        color: ${({ selected }) => selected ? 'white' : 'inherit'};
+        border-radius: ${({ selected }) => selected ? '16px' : 'inherit'};
 
-const NavBarItems = styled.div`
-a {
-    width: 33.3%;
-    color: white;
-    text-decoration: none;
-}
-`
+        & + & {
+            padding: ${({ selected }) => selected ? '0' : '20px 0'};
+        }
+    `;
 
-const Navbar = () => {
+const NavbarContainer = () => {
+    const [selectedItem, setSelectedItem] = useState(null);
+
+    const handleItemClick = (item) => {
+        setSelectedItem(prevSelectedItem => prevSelectedItem === item ? null : item);
+    }
+
+    const generateNavBarItems = () => {
+        return navItems.map((item, index) => (
+            <Link to={`/${item.className}`}>
+                <NavBarItems
+                    key={index}
+                    selected={selectedItem === item.className}
+                    className={item.className + (selectedItem === item.className ? ' selected' : '')}
+                    onClick={() => handleItemClick(item.className)}
+                >
+                    {item.text}
+                </NavBarItems>
+            </Link>
+        ));
+    }
+
+    const navItems = [
+        { className: 'tournaments', text: 'Tournaments' },
+        { className: 'matches', text: 'Matches' },
+        { className: 'odds', text: 'Odds' },
+    ];
+
     return (
-        <NavBar>
-            <NavBarItems className="tournaments">
-                <Link to="/tournaments">Tournaments</Link>
-            </NavBarItems>
-            <NavBarItems className="matches">
-                <Link to="/matches">Matches</Link>
-            </NavBarItems>
-            <NavBarItems className="odds">
-                <Link to="/odds">Odds</Link>
-            </NavBarItems>
+        <NavBar className="NavBar">
+            {generateNavBarItems()}
         </NavBar>
-    )
+    );
 }
 
-export default Navbar;
+export default NavbarContainer;
